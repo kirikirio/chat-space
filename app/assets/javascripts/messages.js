6e6c679cd428c83ae319edb9960b401e9a22243a
kirikirio <kirikiri3926@gmail.com>
2019-10-24 10:48:12 +0900

    lgtm.happy

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html =  `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    <div>
+                      ${message.text}
+                    </div>
+                      ${img}
+                  </p>
+                </div>`
+        return html ;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    $('#ajax').removeAttr('data-disable-with');
+    var url = $(this).attr('action')
+    $('#new_message')[0].reset();
+    
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      data.id
+      data.content
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('エラーが発生しています');
+    })
+  })
+});
