c4d60a569ec7138029a5242279f8e462c82a07d2
kirikirio <kirikiri3926@gmail.com>
2019-10-26 14:48:22 +0900

    コンソールログ

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -44,29 +44,19 @@ $(function(){
     })
   });
     var reloadMessages = function() {
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       last_message_id = $('.message').last().data("id");
-      console.log(last_message_id);
       $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: 'api/messages',
-        //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
-        //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
       })
       .done(function(messages) {
-        console.log(messages);
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
       messages.forEach(function (message) {
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       insertHTML = buildHTML(message);
-      //メッセージが入ったHTMLを取得
       $('.messages').append(insertHTML);
       $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight});
-      //メッセージを追加
       })
       .fail(function() {
         console.log('error');
