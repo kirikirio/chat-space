ca5184fda076ee2523f43fb18565c6451b825be5
kirikirio <kirikiri3926@gmail.com>
2019-10-26 14:38:27 +0900

    できた

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,18 +1,18 @@
 $(function(){
   function buildHTML(message){
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html =  `<div class="message">
+    var html =  `<div class="message" data-id="${message.id}" >
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
                     </p>
                     <p class="message__upper-info__date">
-                      ${message.date}
+                      ${message.created_at}
                     </p>
                   </div>
                   <p class="message__text">
                     <div>
-                      ${message.text}
+                      ${message.content}
                     </div>
                       ${img}
                   </p>
@@ -42,5 +42,36 @@ $(function(){
     .fail(function(){
       alert('エラーが発生しています');
     })
-  })
+  });
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message').last().data("id");
+      console.log(last_message_id);
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: 'api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log(messages);
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      messages.forEach(function (message) {
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      insertHTML = buildHTML(message);
+      //メッセージが入ったHTMLを取得
+      $('.messages').append(insertHTML);
+      $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight});
+      //メッセージを追加
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,6 @@
-json.id         @message.id
-json.text       @message.content
-json.date       @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.user_name  @message.user.name
-json.image      @message.image.url
+json.content @message.content
+json.image @message.image.url
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
+#idもデータとして渡す
+json.id @message.id
